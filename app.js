5bcebd88eb4e02b8aa1b1c200ad4c0421d7ce986
Andrew Cer <cer.andrew@gmail.com>
2015-07-31 13:23:59 -0600

    setting up env for client ids

--- app.js
+++ app.js
@@ -2,9 +2,9 @@ var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
+require('dotenv').load();
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
