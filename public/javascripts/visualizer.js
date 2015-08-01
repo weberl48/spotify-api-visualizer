29adbc7d23e6856d7ab8d046dc25c036ad4a9f92
Lucas Weber <weberl48@outlook.com>
2015-08-01 16:18:22 -0600

    updates

--- public/javascripts/visualizer.js
+++ public/javascripts/visualizer.js
@@ -1,68 +1,84 @@
-var AudioContext = window.AudioContext || window.webkitAudioContext;
-var audioCtx = new AudioContext();
-
+var AudioContext = window.AudioContext || window.webkitAudioContext; // an audio-processing graph built from audio modules linked together
+var audioCtx = new AudioContext(); // an audio-processing graph built from audio modules linked together
+var audio = document.getElementById('myAudio');
+console.log(audio);
 var canvas = document.getElementById('myCanvas');
 var ctx = canvas.getContext('2d');
-
 var drawVisual; // requestAnimationFrame
-
 var analyser = audioCtx.createAnalyser();
+  // creates an AnalyserNode, which can be used to expose audio time and -
+  // - frequency data and create data visualisations.
 var gainNode = audioCtx.createGain();
+  // creates a GainNode, which can be used to control the overall volume of the audio graph.
 
-
-navigator.webkitGetUserMedia (
-  {
-    audio: true
-  },
+//access mic
+// navigator.webkitGetUserMedia (
+//   {
+//     audio: audio
+//   },
 
   // Success callback
-  function(stream) {
-    source = audioCtx.createMediaStreamSource(stream);
+  function getAudio(audio) {
+    //used to create a new MediaStreamAudioSourceNode object, given a media stream
+    // source = audioCtx.createMediaStreamSource(stream); //convert into useable object
+    source = audioCtx.createMediaStreamSource(audio);
+    //connect the analyser
+    // and the gainNode to the destination, so we can play the
+    // music and adjust the volume using the mouse cursor
     source.connect(analyser);
     gainNode.connect(audioCtx.destination); // connecting the different audio graph nodes together
-    visualize(stream);
+    visualize(audio); //
 
-  },
-
-  // Error callback
-  function(err) {
-    console.log('The following gUM error occured: ' + err);
   }
-);
 
-function visualize(stream) {
+//   // Error callback
+//   function(err) {
+//     console.log('The following gUM error occured: ' + err);
+//   }
+// );
+
+function visualize(audio) {
   WIDTH = canvas.width;
   HEIGHT = canvas.height;
 
-
+//(Fast Fourier Transform) to be used to determine the frequency domain.
   analyser.fftSize = 256;
-    var bufferLength = analyser.frequencyBinCount;
+    var bufferLength = analyser.frequencyBinCount; //unsigned long value half that of the FFT size :128
     console.log(bufferLength);
     var dataArray = new Uint8Array(bufferLength);
-
+    console.log(dataArray);
     ctx.clearRect(0, 0, WIDTH, HEIGHT);
 
+    function getRandomColor() {
+    var letters = '0123456789ABCDEF'.split('');
+    var color = '#';
+    for (var i = 0; i < 6; i++ ) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+
+    return color;
+}
+
+
     function draw() {
-      drawVisual = requestAnimationFrame(draw);
-
-      analyser.getByteFrequencyData(dataArray);
-
+      drawVisual = requestAnimationFrame(draw); //tells the browser that you wish to perform an animation
+      analyser.getByteFrequencyData(dataArray);//method of the Analyser copies the current frequency data into a Uint8Array (unsigned byte array) passed into it.
       ctx.fillStyle = 'rgb(0, 0, 0)';
       ctx.fillRect(0, 0, WIDTH, HEIGHT);
 
-      var barWidth = (WIDTH / bufferLength) * 2.5;
+      var barWidth = (WIDTH / bufferLength) * 2.5; // stretches accros length of canvas
       var barHeight;
       var x = 0;
 
       for(var i = 0; i < bufferLength; i++) {
         barHeight = dataArray[i];
+        // ctx.fillStyle = 'rgb(' + (barHeight+100) + ',50,50)'; //different shades of red
+        ctx.fillStyle = getRandomColor();
+        ctx.fillRect(x,HEIGHT-barHeight/2,barWidth,barHeight/2); // height verticle positioning
 
-        ctx.fillStyle = 'rgb(' + (barHeight+100) + ',50,50)';
-        ctx.fillRect(x,HEIGHT-barHeight/2,barWidth,barHeight/2);
-
-        x += barWidth + 1;
+        x += barWidth + 20; //space inbetween bars
       }
     }
-
+    getAudio(audio);
     draw();
 }
