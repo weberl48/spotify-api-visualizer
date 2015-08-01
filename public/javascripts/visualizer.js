d7fea0e70bb29f5c03fd19f58f8aef4ff238e93a
Sam Genduso <sam.genduso@gmail.com>
2015-08-01 16:33:53 -0600

    visualizer of local song

--- public/javascripts/visualizer.js
+++ public/javascripts/visualizer.js
@@ -1,13 +1,8 @@
-var AudioContext = window.AudioContext || window.webkitAudioContext;
-var audioCtx = new AudioContext();
 
 var canvas = document.getElementById('myCanvas');
 var ctx = canvas.getContext('2d');
 
 var drawVisual; // requestAnimationFrame
-
-var analyser = audioCtx.createAnalyser();
-var gainNode = audioCtx.createGain();
 
 var randomColor = function () {
 	return 'rgb(' + Math.round(Math.random() * 255) + ', ' + Math.round(Math.random() * 255) + ', ' + Math.round(Math.random() * 255) + ')';
@@ -19,64 +14,126 @@ var blackAndWhite = function () {
 };
 
 
-navigator.webkitGetUserMedia (
-  {
-    audio: true
-  },
+// navigator.webkitGetUserMedia (
+//   {
+//     audio: true
+//   },
+//
+//   // Success callback
+//   function(stream) {
+//     var AudioContext = window.AudioContext || window.webkitAudioContext;
+//     var audioCtx = new AudioContext();
+//     var analyser = audioCtx.createAnalyser();
+//     var gainNode = audioCtx.createGain();
+//     source = audioCtx.createMediaStreamSource(stream);
+//     source.connect(analyser);
+//     gainNode.connect(audioCtx.destination); // connecting the different audio graph nodes together
+//     visualizeMic(stream);
+//
+//   },
+//
+//   // Error callback
+//   function(err) {
+//     console.log('The following gUM error occured: ' + err);
+//   }
+// );
+//
+// function visualizeMic(stream) {
+//   WIDTH = canvas.width;
+//   HEIGHT = canvas.height;
+//
+//
+//   analyser.fftSize = 1024;
+//     var bufferLength = analyser.frequencyBinCount;
+//     console.log(bufferLength);
+//     var dataArray = new Uint8Array(bufferLength);
+//
+//     ctx.clearRect(0, 0, WIDTH, HEIGHT);
+//
+//     function draw() {
+//       drawVisual = requestAnimationFrame(draw);
+//
+//       analyser.getByteFrequencyData(dataArray);
+//       console.log(dataArray);
+//
+//       ctx.fillStyle = 'rgb(0, 0, 0)';
+//       ctx.fillRect(0, 0, WIDTH, HEIGHT);
+//
+//       var barWidth = (WIDTH / bufferLength) * 2.5;
+//       var barHeight;
+//       var x = 0;
+//
+//       for(var i = 0; i < bufferLength; i++) {
+//         barHeight = dataArray[i];
+//         // shades of green:
+//         // ctx.fillStyle = 'rgb(50,' + (barHeight+100) + ',50)';
+//         //shades of grey:
+//         // ctx.fillStyle = blackAndWhite();
+//         //crazebow:
+//         ctx.fillStyle = randomColor();
+//         ctx.fillRect(x,HEIGHT-barHeight/2,barWidth,barHeight/2);
+//
+//         x += barWidth + 1;
+//       }
+//     }
+//
+//     draw();
+// }
 
-  // Success callback
-  function(stream) {
-    source = audioCtx.createMediaStreamSource(stream);
+
+  function bufferSound() {
+    // var audio = new Audio();
+    var audio = document.getElementById('player');
+    audio.src = '../sample.mp3';
+    var context = new webkitAudioContext();
+    var analyser = context.createAnalyser();
+    var source = context.createMediaElementSource(audio);
     source.connect(analyser);
-    gainNode.connect(audioCtx.destination); // connecting the different audio graph nodes together
-    visualize(stream);
+    analyser.connect(context.destination);
 
-  },
-
-  // Error callback
-  function(err) {
-    console.log('The following gUM error occured: ' + err);
-  }
-);
-
-function visualize(stream) {
-  WIDTH = canvas.width;
-  HEIGHT = canvas.height;
+    function visualizeSong() {
+      WIDTH = canvas.width;
+      HEIGHT = canvas.height;
 
 
-  analyser.fftSize = 1024;
-    var bufferLength = analyser.frequencyBinCount;
-    console.log(bufferLength);
-    var dataArray = new Uint8Array(bufferLength);
+      analyser.fftSize = 1024;
+        var bufferLength = analyser.frequencyBinCount;
+        console.log(bufferLength);
+        var dataArray = new Uint8Array(bufferLength);
 
-    ctx.clearRect(0, 0, WIDTH, HEIGHT);
+        ctx.clearRect(0, 0, WIDTH, HEIGHT);
 
-    function draw() {
-      drawVisual = requestAnimationFrame(draw);
+        function draw() {
+          drawVisual = requestAnimationFrame(draw);
 
-      analyser.getByteFrequencyData(dataArray);
-      console.log(dataArray);
+          analyser.getByteFrequencyData(dataArray);
+          console.log(dataArray);
 
-      ctx.fillStyle = 'rgb(0, 0, 0)';
-      ctx.fillRect(0, 0, WIDTH, HEIGHT);
+          ctx.fillStyle = 'rgb(0, 0, 0)';
+          ctx.fillRect(0, 0, WIDTH, HEIGHT);
 
-      var barWidth = (WIDTH / bufferLength) * 2.5;
-      var barHeight;
-      var x = 0;
+          var barWidth = (WIDTH / bufferLength) * 2.5;
+          var barHeight;
+          var x = 0;
 
-      for(var i = 0; i < bufferLength; i++) {
-        barHeight = dataArray[i];
-        //shades of green:
-        // ctx.fillStyle = 'rgb(50,' + (barHeight+100) + ',50)';
-        //shades of grey:
-        ctx.fillStyle = blackAndWhite();
-        //crazebow:
-        // ctx.fillStyle = randomColor();
-        ctx.fillRect(x,HEIGHT-barHeight/2,barWidth,barHeight/2);
+          for(var i = 0; i < bufferLength; i++) {
+            barHeight = dataArray[i];
+            // shades of green:
+            ctx.fillStyle = 'rgb(50,' + (barHeight+100) + ',50)';
+            //shades of grey:
+            // ctx.fillStyle = blackAndWhite();
+            //crazebow:
+            // ctx.fillStyle = randomColor();
+            ctx.fillRect(x,HEIGHT-barHeight/2,barWidth,barHeight/2);
 
-        x += barWidth + 1;
-      }
+            x += barWidth + 1;
+          }
+        }
+
+        draw();
     }
 
-    draw();
-}
+    visualizeSong();
+  }
+
+bufferSound();
